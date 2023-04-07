2fd7157710554be03633223ce1cb4f1850b9f8fd
jin-vita <jhj9144@gmail.com>
2023-04-07 09:00:37 +0900

    try1

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,8 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:my_warp/util/my_painter.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,10 +14,42 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    String imagePath = 'assets/images/gmail.png';
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('warp image'),
       ),
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: Image(
+              image: AssetImage(imagePath),
+            ),
+          ),
+          FutureBuilder(
+            future: loadImageFromAsset(imagePath),
+            builder: (BuildContext context, AsyncSnapshot<ui.Image> snapshot) {
+              if (snapshot.hasData) {
+                return CustomPaint(
+                  painter: ImagePainter(snapshot.data!),
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ],
+      ),
     );
+  }
+
+  Future<ui.Image> loadImageFromAsset(String path) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List());
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return fi.image;
   }
 }
--- lib/util/my_painter.dart
+++ lib/util/my_painter.dart
@@ -2,24 +2,58 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 
-class MyPainter extends CustomPainter {
-  final ui.Image image;
-  final List<Offset> points;
+class ImagePainter extends CustomPainter {
+  ui.Image image;
+  List<Offset> points = [];
 
-  MyPainter(this.image, this.points);
+  ImagePainter(this.image) {
+    // 이미지의 중심점 계산
+    Offset center = Offset(image.width / 2, image.height / 2);
+
+    // 이미지 위에 점 4개 찍기
+    points.add(center.translate(-100, -50));
+    points.add(center.translate(50, 0));
+    points.add(center.translate(-50, 50));
+    points.add(center.translate(50, 50));
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawImage(image, Offset.zero, Paint());
+    // 이미지의 변형 계산
+    Matrix4 matrix = Matrix4.identity();
+    matrix.setValues(
+      points[0].dx,
+      points[0].dy,
+      0,
+      0,
+      points[1].dx,
+      points[1].dy,
+      0,
+      0,
+      points[2].dx,
+      points[2].dy,
+      0,
+      0,
+      points[3].dx,
+      points[3].dy,
+      0,
+      0,
+    );
 
-    final paint = Paint()..color = Colors.red;
-    for (var i = 0; i < points.length; i++) {
-      canvas.drawCircle(points[i] * size.width, 10, paint);
+    // 이미지 변형 적용
+    canvas.save();
+    canvas.transform(matrix.storage);
+    canvas.drawImage(image, Offset.zero, Paint());
+    canvas.restore();
+
+    // 점 그리기
+    for (var point in points) {
+      canvas.drawCircle(point, 5, Paint()..color = Colors.red);
     }
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+  bool shouldRepaint(ImagePainter oldDelegate) {
+    return true;
   }
 }
